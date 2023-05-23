5c274e3cb74dce083e36657eaf81c96e0594ad76
Mário Neto <73663266+marioprneto@users.noreply.github.com>
2023-05-22 22:38:39 -0300

    Criado o incluirusuario

--- trabalho.c
+++ trabalho.c
@@ -131,3 +131,130 @@ int menu(int quantidade){
     }
 }
 
+void incluirUsuarios(){
+    int i = 0, valorVacina, contadorPergunta = 0;
+    char *encerrarAlgoritmo;
+    float valorAltura;
+
+    getchar();
+
+    while(i<2){
+        
+        while(verificaNumero(ids,i,geraID(ids,i)) == 1){
+            geraID(ids,i);
+        }
+
+        ids[i] = geraID(ids,i);
+
+        printf("Digite o nome do usuário %d: ",i);
+        fgets(nomeCompleto[i], 100, stdin);
+
+        nomeCompleto[i][strcspn(nomeCompleto[i], "\n")] = '\0';
+
+        printf("Digite o e-mail do usuário %d: ",i);
+        fgets(email[i], 100, stdin);
+
+        while(strchr(email[i],'@') == NULL){
+            printf("Digitação inválida! Digite um e-mail válido: ");
+            fgets(email[i], 100, stdin);
+            email[i][strcspn(email[i], "\n")] = '\0';
+        }
+
+        email[i][strcspn(email[i], "\n")] = '\0';
+
+        printf("Digite o sexo do usuário (Feminino, Masculino e Indiferente) %d: ",i);
+        fgets(sexo[i], 100, stdin);
+        sexo[i][strcspn(sexo[i], "\n")] = '\0';
+
+        while(strcmp(sexo[i],"Feminino")!=0 && strcmp(sexo[i],"Masculino")!=0 && strcmp(sexo[i],"Indiferente")!=0){
+            printf("Digitação inválida! Digite conforme está emntre parenteses. (Feminino, Masculino e Indiferente): ",i);
+            fgets(sexo[i], 100, stdin);
+            sexo[i][strcspn(sexo[i], "\n")] = '\0';
+        }
+
+        printf("Digite o endereço do usuário %d: ",i);
+        fgets(endereco[i], 100, stdin);
+
+        endereco[i][strcspn(endereco[i], "\n")] = '\0';
+
+        printf("Digite a altura do usuário %d: ",i);
+        scanf("%f",&valorAltura);
+
+        while(valorAltura < 1 || valorAltura > 2){
+            printf("Altura inválida! Digite um valor válido entre 1 e 2 metros: ");
+            scanf("%f",&valorAltura);
+        }
+
+        altura[i] = valorAltura;
+
+        printf("Digite (1) para se o usuário tomou vacina ou (0) se o usuário não tomou vacina: ");
+        scanf("%i",&valorVacina);
+
+        while (valorVacina != 0 && valorVacina != 1)
+        {
+            printf("Digite (1) para se o usuário tomou vacina ou (0) se o usuário não tomou vacina: ");
+            scanf("%i",&valorVacina);
+        }
+
+        vacina[i] = valorVacina;
+
+        getchar();
+
+        /*if(contadorPergunta == 0){
+            printf("\nDeseja parar de incluir usuários agora? (Sim) para sim, (Nao) para não. ");
+            scanf("%s",&encerrarAlgoritmo);       
+            encerrarAlgoritmo[strcspn(encerrarAlgoritmo, "\n")] = '\0';
+
+            if(strcmp(encerrarAlgoritmo,"Sim")==0){
+                break;
+                i = 1000;
+            } else{
+                printf("Deseja incluir quantos funcionários no mínimo até que eu faça essa pergunta novamente? ");
+                scanf("%i",&contadorPergunta);
+                while (contadorPergunta <= 0)
+                {
+                    printf("Valor digitado inválido! Deseja incluir quantos funcionários no mínimo até que eu faça essa pergunta novamente? ");
+                    scanf("%i",&contadorPergunta);
+                }
+            }
+        }*/
+        
+        contadorPergunta--;
+        i++;
+
+        getchar();
+    }
+    
+    menu(i);
+    return 0;
+}
+
+
+int geraID(ids, i){
+    int numeroGerado;
+    numeroGerado = rand();
+    while(verificaNumero(ids,i,numeroGerado) == 1){
+        numeroGerado = rand();
+    }
+    return numeroGerado;
+}
+
+int verificaNumero(int vetor[], int tamanho, int numero) {
+    for (int i = 0; i < tamanho; i++) {
+        if (vetor[i] == numero) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int retornaTamanhoVetor(){
+    int contador = 0;
+    for(int j=0;j<1000;j++){
+        if(altura[j]>=1){
+            contador++;
+        } else{
+            return contador;
+        }
+    }
+}
